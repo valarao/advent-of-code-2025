ab5b985b2494116472f5827db6d6bd183660e857
Miguel Valarao <miguel.valarao@gmail.com>
2025-11-30 22:26:56 -0800

    Day 1 solution

--- src/solutions/day_01.py
+++ src/solutions/day_01.py
@@ -7,9 +7,39 @@ from base import Solution
 
 
 class Solution01(Solution):
-
     def part1(self) -> int:
-        pass
+        counts = 0
+        dial_number = 50
+
+        for x in self.input_data:
+            if x[0] == "L":
+                dial_number = (dial_number - int(x[1:])) % 100
+            else:
+                dial_number = (dial_number + int(x[1:])) % 100
+            if dial_number == 0:
+                counts += 1
+
+        return counts
 
     def part2(self) -> int:
-        pass
+        counts = 0
+        dial_number = 50
+
+        for x in self.input_data:
+            direction = x[0]
+            rotation = int(x[1:])
+
+            if direction == "L":
+                counts += rotation // 100
+                remainder = rotation % 100
+                if dial_number > 0 and remainder >= dial_number:
+                    counts += 1
+                dial_number = (dial_number - rotation) % 100
+            else:
+                counts += rotation // 100
+                remainder = rotation % 100
+                if remainder > 0 and remainder >= (100 - dial_number):
+                    counts += 1
+                dial_number = (dial_number + rotation) % 100
+
+        return counts
