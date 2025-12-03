5b97b03e7c580a80c76e8dfb0de867f202c7675d
Miguel Valarao <miguel.valarao@gmail.com>
2025-12-02 21:46:04 -0800

    Day 3 solution

--- src/solutions/day_03.py
+++ src/solutions/day_03.py
@@ -7,9 +7,53 @@ from base import Solution
 
 
 class Solution03(Solution):
-
     def part1(self) -> int:
-        pass
+        total = 0
+        for bank in self.input_data:
+            voltage = self.get_voltage_with_two_digits(bank)
+            total += int(voltage)
+        return total
 
     def part2(self) -> int:
-        pass
+        total = 0
+        for bank in self.input_data:
+            voltage = self.get_voltage_with_twelve_digits(bank)
+            total += int(voltage)
+        return total
+
+    def get_voltage_with_two_digits(self, bank):
+        max_1 = (-1, -1)
+        max_2 = (-1, -1)
+        for idx, battery in enumerate(bank[:-1]):
+            if int(battery) > int(max_1[0]):
+                max_1 = (battery, idx)
+
+        start = max_1[1] + 1
+        for idx in range(start, len(bank)):
+            battery = bank[idx]
+            if int(battery) > int(max_2[0]):
+                max_2 = (battery, idx)
+
+        return int(str(max_1[0]) + str(max_2[0]))
+
+    def get_voltage_with_twelve_digits(self, bank):
+        digits = 12
+        batteries = []
+        start = 0
+
+        for _ in range(digits):
+            remaining = digits - len(batteries) - 1
+            end = len(bank) - remaining
+
+            max_digit = (-1, -1)
+
+            for idx in range(start, end):
+                battery = bank[idx]
+                if int(battery) > int(max_digit[0]):
+                    max_digit = (battery, idx)
+
+            batteries.append(str(max_digit[0]))
+
+            start = max_digit[1] + 1
+
+        return int("".join(batteries))
