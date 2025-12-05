4488a541c769cabe356dfd92d53d83c088ff5eac
Miguel Valarao <miguel.valarao@gmail.com>
2025-12-05 00:39:49 -0800

    Refactor for funsies

--- src/solutions/day_01.py
+++ src/solutions/day_01.py
@@ -23,23 +23,37 @@ class Solution01(Solution):
 
     def part2(self) -> int:
         counts = 0
-        dial_number = 50
+        dial = 50
 
-        for x in self.input_data:
-            direction = x[0]
-            rotation = int(x[1:])
+        for rotation_info in self.input_data:
+            direction, rotation = self.parse_rotation_info(rotation_info)
+            full_turns = rotation // 100
+            remainder = rotation % 100
 
-            if direction == "L":
-                counts += rotation // 100
-                remainder = rotation % 100
-                if dial_number > 0 and remainder >= dial_number:
-                    counts += 1
-                dial_number = (dial_number - rotation) % 100
-            else:
-                counts += rotation // 100
-                remainder = rotation % 100
-                if remainder > 0 and remainder >= (100 - dial_number):
-                    counts += 1
-                dial_number = (dial_number + rotation) % 100
+            counts += full_turns
+            if self.has_additional_click(remainder, direction, dial):
+                counts += 1
+
+            dial = self.update_dial(direction, dial, rotation)
 
         return counts
+
+    def parse_rotation_info(self, rotation_info):
+        return (rotation_info[0], int(rotation_info[1:]))
+
+    def has_additional_click(self, remainder, direction, dial):
+        if remainder == 0:
+            return False
+
+        if direction == "L":
+            distance_to_zero = dial
+        else:
+            distance_to_zero = 100 - dial
+
+        return remainder >= distance_to_zero and distance_to_zero > 0
+
+    def update_dial(self, direction, dial, rotation):
+        if direction == "L":
+            return (dial - rotation) % 100
+        else:
+            return (dial + rotation) % 100
